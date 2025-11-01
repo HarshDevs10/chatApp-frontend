fcc32dbf7a74f800689f50cd18e5ef02ad96fef2
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-11-01 17:34:08 +0000

    connection with ws

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState, type RefObject } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [message, setMessage] = useState<string[]>([])
+  const [ws, setWs] = useState<WebSocket>()
+  const Ref1 = useRef(null)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const SendMessage = () => {
+    //@ts-ignore
+    ws?.send(Ref1.current.value)
+  } 
+  
+  useEffect(() => {
+    const Socket = new WebSocket("ws://localhost:8000")
+    setWs(Socket)
+
+    Socket.onmessage = (e) => {
+      console.log(e.data)
+      setMessage(prev => [...prev, e.data])
+    }
+
+    return () => Socket.close()
+  }, [])
+
+  return <div style={{justifyContent: "center", alignItems: "center"}}>
+    <div style={{display: "flex", justifyContent: "center"}}>
+      {message}
+    </div>
+    <input type="text" ref={Ref1} placeholder="Message....." />
+    <button onClick={SendMessage}>Send</button>
+  </div>
 }
 
 export default App
