1aa43a1068719bb35d787e6993bbfa247312e4f4
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-11-09 17:41:52 +0000

    Style added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef, useState } from "react"
 
 const App = () => {
-  const [message, setMessage] = useState<string[]>([])
+  const [message, setMessage] = useState<string[]>(["hello world", "singh"])
   const [ws, setWs] = useState<WebSocket>()
   const Ref1 = useRef(null)
 
   const SendMessage = () => {
+    if(!Ref1.current) return
     //@ts-ignore
     ws?.send(Ref1.current.value)
   } 
@@ -22,12 +23,14 @@ const App = () => {
     return () => Socket.close()
   }, [])
 
-  return <div className="flex justify-center items-center h-screen">
-    <div>
-      {message}
+  return <div className="flex flex-col w-96 h-screen bg-amber-800 justify-center items-center">
+    <div className="">
+      {message.map(mes => <div className="m-2"><span className="bg-white text-black rounded-lg p-1 font-medium">{mes}</span></div>)}
     </div>
-    <input type="text" className="bg-black text-white" ref={Ref1} placeholder="Message....." />
-    <button onClick={SendMessage}>Send</button>
+    <div>
+      <input ref={Ref1} type="text" placeholder="Message..." className="bg-white text-slate-800 focus:outline-indigo-500"/>
+      <button onClick={SendMessage} className="bg-purple-600 text-white cursor-pointer ml-2 rounded-xs">Send</button>
+    </div>
   </div>
 }
 
